8e0424d8a68470129e9a04a3361bcc7015dc0946
Arjan Aswal <arjanaswal09@gmail.com>
2022-05-03 00:25:41 +0530

    Create requirement model

---
+++ src/models/requirementModel.js
@@ -0,0 +1,21 @@
+const { Schema, model } = require('mongoose');
+
+const RequirementSchema = new Schema({
+  title: {
+    type: String,
+    required: [true, 'Title cannot be empty!'],
+    trim: true,
+    maxlength: [50, 'Title cannot be longer than 50 characters!'],
+  },
+  description: {
+    type: String,
+    trim: true,
+    maxlength: [1000, 'Description cannot be longer than 1000 characters!'],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+module.exports = model('Requirement', RequirementSchema);
