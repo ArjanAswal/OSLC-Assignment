8baa38810804d8093a28975cd359f62654c8b949
Arjan Aswal <arjanaswal09@gmail.com>
2022-05-03 00:16:37 +0530

    Create a logger

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const logger = require('./utils/logger');
+
 const app = express();
 
 app.listen(process.env.PORT ?? 3000, () => {
-  console.log('Server started on port 3000');
+  logger.log('Server started on port 3000');
 });
 
 app.get('/', (req, res) => {
