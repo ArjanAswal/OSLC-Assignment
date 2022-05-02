86c7ba312c933cba90657c01ce4d601de3d496a2
Arjan Aswal <arjanaswal09@gmail.com>
2022-05-03 00:18:39 +0530

    Connect mongoDB

--- src/server.js
+++ src/server.js
@@ -1,12 +1,42 @@
-const express = require('express');
+const app = require('./app');
+const mongoose = require('mongoose');
 const logger = require('./utils/logger');
 
-const app = express();
-
-app.listen(process.env.PORT ?? 3000, () => {
-  logger.log('Server started on port 3000');
+process.on('uncaughtException', (err) => {
+  logger.error('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  logger.error(err);
+  process.exit(1);
 });
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+mongoose.connect(process.env.MONGO_URL).then(() => {
+  logger.info('MongoDB connection successful!');
+});
+
+const port = process.env.PORT ?? 80;
+const server = app.listen(port, () => {
+  logger.info(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', (err) => {
+  logger.error('UNHANDLED REJECTION! 💥 Shutting down...');
+  logger.error(err);
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+// quit on ctrl-c when running docker in terminal
+process.on('SIGINT', function onSigint() {
+  logger.info('👋 SIGINT RECEIVED. Shutting down gracefully');
+  server.close(() => {
+    logger.info('💥 Process terminated!');
+  });
+});
+
+// quit properly on docker stop
+process.on('SIGTERM', () => {
+  logger.info('👋 SIGTERM RECEIVED. Shutting down gracefully');
+  server.close(() => {
+    logger.info('💥 Process terminated!');
+  });
 });
